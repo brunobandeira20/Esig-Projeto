2cad12728eef3841254aeb03c2c075e96d5f2949
brunobandeira20 <brunobandeira20@gmail.com>
2022-02-16 20:27:35 -0300

    finish beans

--- src/beans/CadastrarTarefaBean.java
+++ src/beans/CadastrarTarefaBean.java
@@ -1,17 +1,18 @@
 package beans;
 
-
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.EntityManager;
 
+import dao.Database;
 import domains.Tarefa;
+import resources.MetodosResource;
+
 
 @ManagedBean
 @RequestScoped
 public class CadastrarTarefaBean {
-	private Tarefa tarefa ;
-	
+	private Tarefa tarefa;
 
 	public CadastrarTarefaBean() {
 		tarefa = new Tarefa();
@@ -25,5 +26,68 @@ public class CadastrarTarefaBean {
 		this.tarefa = tarefa;
 	}
 
+	public String entrarEdicaotarefas(Tarefa tarefa) {
+		this.tarefa = tarefa; 
+		return "";
+	}
+
+	public String editarTarefa(Tarefa tarefa) {
+		this.tarefa = tarefa; 
+		return "";
+	}
+	public String cadastrar() {
+		EntityManager gerenciador = Database.getInstance().getEntityManager();
+		gerenciador.getTransaction().begin();
+		boolean erro = false;
+
+		if (MetodosResource.estaVazio(tarefa.getTitulo())) {
+			MetodosResource.addMensagem("Campo Título obrigatório!");
+			erro = true;
+		}
+		if (MetodosResource.estaVazio(tarefa.getDescricao())) {
+			MetodosResource.addMensagem("Campo Descrição obrigatório!");
+			erro = true;
+		}
+		if (MetodosResource.estaVazio(tarefa.getPrioridade())) {
+			MetodosResource.addMensagem("Campo Prioridade obrigatório!");
+			erro = true;
+		}
+		if (MetodosResource.estaVazio(tarefa.getResponsavel())) {
+			MetodosResource.addMensagem("Campo Responsável obrigatório!");
+			erro = true;
+		}
+		if (tarefa.getDeadline() == null) {
+			MetodosResource.addMensagem("Campo Deadline obrigatório!");
+			erro = true;
+		}
+		if (erro) {
+			if (gerenciador.getTransaction().isActive()) {
+				gerenciador.getTransaction().rollback();
+			}
+			return null;
+		} else {
+			try {
+				if (tarefa.getId() == 0)
+					gerenciador.persist(tarefa);
+				else
+					gerenciador.merge(tarefa);
+
+				gerenciador.getTransaction().commit();
+				MetodosResource.addMensagem("Seu cadastro está pronto!");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+				if (gerenciador.getTransaction().isActive()) {
+					gerenciador.getTransaction().rollback();
+				}
+			}
+
+			tarefa = new Tarefa();
+			return null;
+		}
+
+	}
 	
+
 }
